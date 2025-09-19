e0a817d4c37dded42b7089d8e5a86c72be0272d0
Your Name <you@example.com>
2025-09-19 13:41:10 +0300

    form added

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const p1 = document.getElementById("p1");
 const p2 = document.getElementById("p2");
 const p3 = document.getElementById("p3");
 const p4 = document.getElementById("p4");
+const lists = document.getElementById("lists");
+const forma = document.getElementById("forma");
 
 button.addEventListener("click", postClick);
 
 async function postClick() {
+  const formData = new FormData(forma);
+  const vertiba = formData.get("asd");
+  console.log(vertiba);
   const url = "https://jsonplaceholder.typicode.com/posts/5";
   const response = await fetch(url);
   const data = await response.json();
