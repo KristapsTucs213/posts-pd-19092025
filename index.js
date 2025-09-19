4c8041cfef53f6f8a49ace378834bfe43623596c
Your Name <you@example.com>
2025-09-19 14:22:43 +0300

    all works

--- index.js
+++ index.js
@@ -11,32 +11,32 @@ async function postClick(event) {
   statuss.innerHTML = "Loading...";
   const formData = new FormData(forma);
   const vertiba = formData.get("asd");
-  console.log(vertiba);
 
-  const url = `https://jsonplaceholder.typicode.com/posts/${vertiba}`;
+  for (let i = 0; i <= vertiba; i++) {
+    const url = `https://jsonplaceholder.typicode.com/posts/${i}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    statuss.innerHTML = "Done!";
+    console.log(data);
 
-  const response = await fetch(url);
-  const data = await response.json();
-  statuss.innerHTML = "Done!";
-  console.log(data);
+    const l1 = document.createElement("li");
+    const l2 = document.createElement("li");
+    const l3 = document.createElement("li");
+    const l4 = document.createElement("li");
 
-  const l1 = document.createElement("li");
-  const l2 = document.createElement("li");
-  const l3 = document.createElement("li");
-  const l4 = document.createElement("li");
+    lists.appendChild(l1);
+    lists.appendChild(l2);
+    lists.appendChild(l3);
+    lists.appendChild(l4);
 
-  lists.appendChild(l1);
-  lists.appendChild(l2);
-  lists.appendChild(l3);
-  lists.appendChild(l4);
+    const user_id = document.createTextNode(data.userId);
+    const idd = document.createTextNode(data.id);
+    const titlee = document.createTextNode(data.title);
+    const boddy = document.createTextNode(data.body);
 
-  const user_id = document.createTextNode(data.userId);
-  const idd = document.createTextNode(data.id);
-  const titlee = document.createTextNode(data.title);
-  const boddy = document.createTextNode(data.body);
-
-  l1.appendChild(user_id);
-  l2.appendChild(idd);
-  l3.appendChild(titlee);
-  l4.appendChild(boddy);
+    l1.appendChild(user_id);
+    l2.appendChild(idd);
+    l3.appendChild(titlee);
+    l4.appendChild(boddy);
+  }
 }
