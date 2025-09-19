5880d47f968c55d24e8c3efb67f05d4dccb2f7db
Your Name <you@example.com>
2025-09-19 13:25:12 +0300

    strada informacijas dabusana

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const p1 = document.getElementById("p1");
 const p2 = document.getElementById("p2");
 const p3 = document.getElementById("p3");
 const p4 = document.getElementById("p4");
+
+button.addEventListener("click", postClick);
+
+async function postClick() {
+  const url = "https://jsonplaceholder.typicode.com/posts/1";
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
