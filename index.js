347eabf0fc8cea462723d60cd35ac469bd9797be
Your Name <you@example.com>
2025-09-19 13:22:47 +0300

    defiend elements

---
+++ index.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("button");
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
+const p3 = document.getElementById("p3");
+const p4 = document.getElementById("p4");
