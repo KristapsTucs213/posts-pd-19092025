fa3134258fab85e6b2dae1ecbc6ff3c9bd1e02d7
Your Name <you@example.com>
2025-09-19 13:45:54 +0300

    form working

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const p3 = document.getElementById("p3");
 const p4 = document.getElementById("p4");
 const lists = document.getElementById("lists");
 const forma = document.getElementById("forma");
+const statuss = document.getElementById("status");
 
 button.addEventListener("click", postClick);
 
 async function postClick() {
+  statuss.innerHTML = "Loading...";
   const formData = new FormData(forma);
   const vertiba = formData.get("asd");
   console.log(vertiba);
   const url = "https://jsonplaceholder.typicode.com/posts/5";
+
+
+  
   const response = await fetch(url);
   const data = await response.json();
+  statuss.innerHTML = "Done!";
   console.log(data);
 
   p1.innerHTML = data.userId;
