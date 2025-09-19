15b830f6e210bcf28962b7dcf969a0a6ffe49887
Your Name <you@example.com>
2025-09-19 13:26:51 +0300

    tagat displayo datus

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const p4 = document.getElementById("p4");
 button.addEventListener("click", postClick);
 
 async function postClick() {
-  const url = "https://jsonplaceholder.typicode.com/posts/1";
+  const url = "https://jsonplaceholder.typicode.com/posts/5";
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
+
+  p1.innerHTML = data.userId;
+  p2.innerHTML = data.id;
+  p3.innerHTML = data.title;
+  p4.innerHTML = data.body;
 }
